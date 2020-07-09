d5af3407fe101b1fcfd7b2abdd02030325e1ee0d
Matthew Kang <ckang@academic.rrc.ca>
2020-07-09 13:33:32 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "ssnotesupload"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://e7jlwpcwuh.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_k1ohTbKDm",
+      APP_CLIENT_ID: "3uh9b2t8sr2ivqmt4lqe415nn4",
+      IDENTITY_POOL_ID: "us-east-2:cde60108-f4f4-446b-b25c-b9e434b90e41"
+    }
+  };
